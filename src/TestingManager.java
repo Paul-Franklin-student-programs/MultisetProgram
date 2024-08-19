c67e2c4eb4edece016ce2aadcfba746e98c4f41b
Paul-Franklin-repositories <pfranklin138@gmail.com>
2024-08-19 11:11:08 -0400

    improved formatting in JUNit testing

--- src/TestingManager.java
+++ src/TestingManager.java
@@ -2,13 +2,14 @@ import org.junit.Before;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
-
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 public class TestingManager {
+
     private Bag <String> testBag1;
+
     private Bag <String> testBag2;
+
     @Before
     public void testSetUp() {
         testBag1 = new Bag();
@@ -22,26 +23,28 @@ public class TestingManager {
         testBag2.add("Godiva Chocolate Bar");
         testBag2.add("Gummy Bears");}
 
-
-
     @org.junit.Test
     public void testAdd() {
         int result = (int) testBag1.treats.getOrDefault("Bubble Gum",0);
         assertEquals(1, result);}
+
     @org.junit.Test
     public void testRemove(){
         testBag1.remove("Bubble Gum");
         int result = (int) testBag1.treats.getOrDefault("Bubble Gum",999);
         assertEquals(999,result);}
+
     @org.junit.Test
     public void testSize(){
         int result = testBag1.size();
         assertEquals(4, result);}
+
     @org.junit.Test
     public void testMerge(){
         testBag1.merge(testBag1,testBag2);
         int result = (int) testBag1.treats.getOrDefault("Godiva Chocolate Bar",999);
         assertEquals(2,result);}
+
     @org.junit.Test
     public void testDistinct(){
         StringBuilder result = testBag2.distinct(testBag2);
